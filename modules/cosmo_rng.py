ab78d79bb79380188413284eb3f3aafd761e1734
Daniel <dalmodovar.1337@gmail.com>
2014-11-19 22:03:03 -0500

    Only online check required for RNG event

--- modules/cosmo_rng.py
+++ modules/cosmo_rng.py
@@ -16,7 +16,7 @@ def alert(event):
 
     last_powerup = settings.trygetset('cosmo_rng_last_powerup', time.time())
     rng_mode = settings.trygetset('cosmo_rng_mode', False)
-    
+
     if last_powerup + 3600 <= time.time() and rng_mode:
         powerup(event.bot)
 
@@ -33,6 +33,9 @@ def alert(event):
                     event.bot.botsay('.timeout %s %s' % (who, duration if duration <= maxto else maxto))
 
 def powerup(bot):
+    if not twitchapi.is_streaming('cosmowright'):
+        return
+
     chatters = twitchapi.get_chatters('cosmowright')['chatters']['viewers']
 
     new_powerups = int(len(chatters) * 0.05)
@@ -45,8 +48,8 @@ def powerup(bot):
 
 def test():
     chatters = twitchapi.get_chatters('cosmowright')['chatters']['viewers']
-    
+
     new_powerups = int(len(chatters) * 0.05)
     new_gods = random.sample(chatters, new_powerups)
 
-    return "Chatters: %s, 5%% = %s, chosen: %s" % (len(chatters), new_powerups, ' ,'.join([str(g) for g in new_gods]))
+    return "(test run) Chatters: %s, 5%% = %s, chosen: %s" % (len(chatters), new_powerups, ', '.join([str(g) for g in new_gods]))
