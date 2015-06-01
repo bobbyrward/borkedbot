9aa56dd95d65873bba8cac0607356f506764feae
Daniel <dalmodovar.1337@gmail.com>
2015-05-31 21:28:04 -0400

    Virtualenv stuff

---
+++ setup.sh
@@ -0,0 +1,3 @@
+virtualenv . --no-site-packages --verbose
+source bin/activate
+pip install dill feedparser github3.py redis requests twisted zerorpc python-dateutil pyopenssl ndg-httpsclient pyasn1 yolk
