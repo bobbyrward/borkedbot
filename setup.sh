6278dd25e46db2e4e4bcdcae99eb8ff2427a3505
Daniel <dalmodovar.1337@gmail.com>
2015-06-28 15:48:38 -0400

    Added rypc to virtualenv setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 virtualenv . --no-site-packages --verbose
 source bin/activate
-pip install dill feedparser github3.py redis requests twisted zerorpc python-dateutil pyopenssl ndg-httpsclient pyasn1 yolk
+pip install dill feedparser github3.py redis requests twisted zerorpc rpyc python-dateutil pyopenssl ndg-httpsclient pyasn1 yolk
